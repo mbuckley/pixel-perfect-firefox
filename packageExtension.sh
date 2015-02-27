cc35f4536dce2be9a3ba4d499135baaae70a9f67
Mike Buckley <mikebuckley.canada@gmail.com>
2015-02-27 08:27:42 -0800

    updated to point to new repo

--- packageExtension.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# generate the XPI file
-# TODO: Pass in version as parameter
-VERSION=1.8.0
-echo "Generating $APP_NAME.xpi..."
-cd srcExtension
-zip -r ../dist/pixelperfect-$VERSION.xpi *
--- src/defaults/preferences/prefs.js
+++
@@ -1,8 +0,0 @@
-pref("extensions.firebug.pixelPerfect.hidestatusbar", false);
-pref("extensions.firebug.pixelPerfect.hidewhenfocuslost", false);
-pref("extensions.firebug.pixelPerfect.lastXPos", '0');
-pref("extensions.firebug.pixelPerfect.lastYPos", '0');
-pref("extensions.firebug.pixelPerfect.opacity", '0.5');
-pref("extensions.firebug.pixelPerfect.zIndex", '1000');
-pref("extensions.firebug.pixelPerfect.overlayLocked", false);
-pref("extensions.firebug.pixelPerfect.lastOverlayFileName", "");
